1febb9a0ea5b2305b2cff578d6d9fe839fa1e668
DenijalSpeech <114922595+DenijalSpeech@users.noreply.github.com>
2025-11-20 08:35:24 +0100

    Update and rename movieApi to movieApi.js

--- api/movieApi.js
+++ api/movieApi.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export async function fetchMovies() {
   const response = await axios.get("https://api.example.com/movies");
+  console.log('check data', response.data)
   return response.data;
 }
